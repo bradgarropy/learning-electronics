ae5ebd09da21cd05822031317bf0c2ec8cca5754
Brad Garropy <bradgarropy@gmail.com>
2026-03-06 22:52:49 -0600

    display messages.

--- 04-lcd-marquee/lcd-marquee.py
+++ 04-lcd-marquee/lcd-marquee.py
@@ -1,26 +1,29 @@
 from RPLCD.i2c import CharLCD
 import time
 
-def setup():
-    lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
-                  cols=16, rows=2, backlight_enabled=True)
+MESSAGES = [
+    ("Hey y'all!", "From ATX"),
+    ("Hello World!", "Raspberry Pi"),
+    ("Learning", "Electronics"),
+    ("LCD1602", "Display Demo"),
+    ("Goodbye!", "See ya later"),
+]
 
-    lcd.write_string('Greetings!')
-    lcd.cursor_pos = (1, 1)  # Row 1, Column 1
-    lcd.write_string('From SunFounder')
-
-    time.sleep(2)
-
-    return lcd
-
-def destroy(lcd):
-    lcd.clear()
+def show_messages(lcd):
+    while True:
+        for line1, line2 in MESSAGES:
+            lcd.clear()
+            lcd.write_string(line1)
+            lcd.cursor_pos = (1, 0)
+            lcd.write_string(line2)
+            time.sleep(5)
 
 if __name__ == "__main__":
     lcd = None
 
     try:
-        lcd = setup()
-    except KeyboardInterrupt:
+        lcd = CharLCD(i2c_expander='PCF8574', address=0x27, cols=16, rows=2)
+        show_messages(lcd)
+    finally:
         if lcd:
-            destroy(lcd)
+            lcd.clear()
